8d11af4b47fcad99ceb1abbcca3e5a60886f3eb9
julio <158296107+jul213@users.noreply.github.com>
2025-06-04 08:35:31 +0000

    bucle para detectar movimiento

--- pir.py
+++ pir.py
@@ -2,3 +2,9 @@ from gpiozero import MotionSensor
 from time import sleep
 
 pir = MotionSensor(4)
+
+while True:
+    pir.wait_for_motion()
+    print("movimiento detectado")
+    pir.wait_for_no_motion()
+    print("sin movimiento...")
